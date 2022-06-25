feff83a099f9d6938328ec5616cf26d4bebeb058
dongliushi <2445264666@qq.com>
2022-06-25 18:12:22 +0800

    mod IR's IcmpInst

--- IR/include/Instructions/IcmpInst.h
+++ IR/include/Instructions/IcmpInst.h
@@ -8,12 +8,15 @@ class Value;
 class IcmpInst : public Instruction {
 public:
   enum class IcmpOp { EQ, NEQ, GT, GTE, LT, LTE };
-  IcmpInst(Type *type, Value *lhs, IcmpOp icmpOp, Value *rhs, BasicBlock *bb);
+  IcmpInst(Type *type, IcmpOp icmpOp, Value *leftValue, Value *rightValue,
+           BasicBlock *insertedBlock);
   IcmpOp getIcmpOp();
-  // static IcmpInst *create_icmp(Module *module, IcmpOp icmpOp, Value *lhs,
-  //                              Value *rhs, BasicBlock *bb);
-  std::string print() override;
   std::string getIcmpOpName();
+  std::string print() override;
+
+public:
+  static IcmpInst *Create(Context &context, IcmpOp icmpOp, Value *leftValue,
+                          Value *rightValue, BasicBlock *insertedBlock);
 
 private:
   IcmpOp icmpOp_;
