f2e3bddc5c9761924a8d4c1f9f58e55ed919faf2
dongliushi <2445264666@qq.com>
2022-06-25 23:33:58 +0800

    mod IR's PhiInst

--- IR/include/Instructions/PhiInst.h
+++ IR/include/Instructions/PhiInst.h
@@ -4,19 +4,15 @@
 
 class PhiInst : public Instruction {
 public:
-  PhiInst(OpId op, std::vector<Value *> vals, std::vector<BasicBlock *> val_bbs,
-          Type *ty, BasicBlock *bb);
-  PhiInst(Type *ty, OpId op, unsigned num_ops, BasicBlock *bb)
-      : Instruction(ty, op, num_ops, bb) {}
-  //   static PhiInst *create_phi(Type *ty, BasicBlock *bb);
-  //   Value *get_lval() { return l_val_; }
-  //   void set_lval(Value *l_val) { l_val_ = l_val; }
-  //   void add_phi_pair_operand(Value *val, Value *pre_bb) {
-  //     this->addOperand(val);
-  //     this->addOperand(pre_bb);
-  //   }
+  PhiInst(Type *type,
+          std::vector<std::pair<Value *, BasicBlock *>> valAndLabels,
+          BasicBlock *insertedBlock);
   std::string print() override;
 
+public:
+  static PhiInst *
+  Create(Type *type, std::vector<std::pair<Value *, BasicBlock *>> valAndLabels,
+         BasicBlock *insertedBlock);
+
 private:
-  Value *l_val_;
 };
--- IR/src/Instructions/PhiInst.cc
+++ IR/src/Instructions/PhiInst.cc
@@ -1,1 +1,41 @@
 #include "Instructions/PhiInst.h"
+#include "BasicBlock.h"
+#include "Util.h"
+
+PhiInst::PhiInst(Type *type,
+                 std::vector<std::pair<Value *, BasicBlock *>> valAndLabels,
+                 BasicBlock *insertedBlock)
+    : Instruction(type, InstId::phi, 2 * valAndLabels.size(), insertedBlock) {
+  for (int i = 0; i < valAndLabels.size(); i++) {
+    setOperand(valAndLabels[i].first, 2 * i);
+    setOperand(valAndLabels[i].second, 2 * i + 1);
+  }
+  insertedBlock->addInstruction(this);
+}
+
+PhiInst *
+PhiInst::Create(Type *type,
+                std::vector<std::pair<Value *, BasicBlock *>> valAndLabels,
+                BasicBlock *insertedBlock) {
+  return new PhiInst(type, valAndLabels, insertedBlock);
+}
+
+std::string PhiInst::print() {
+  std::string IR;
+  char IRtemp[30];
+  // <result> = phi <ty> [ <val0>, <label0>], ...
+  std::string fmt("%%%s = phi %s ");
+  std::snprintf(IRtemp, sizeof IRtemp, fmt.c_str(), getName().c_str(),
+                getOperandType(0)->getTypeName().c_str());
+  IR.assign(IRtemp);
+  for (int i = 0; i < getOperandNum() / 2; i++) {
+    if (i > 0)
+      IR += ", ";
+    IR += "[ ";
+    IR += print_as_op(getOperand(2 * i));
+    IR += ", ";
+    IR += print_as_op(getOperand(2 * i + 1));
+    IR += " ]";
+  }
+  return IR;
+}
