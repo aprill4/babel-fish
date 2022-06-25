bd04915010d62643444a925aef47c26fafeda0b9
dongliushi <2445264666@qq.com>
2022-06-25 22:48:35 +0800

    mod IR's ZextInst

--- IR/src/Instructions/ZextInst.cc
+++ IR/src/Instructions/ZextInst.cc
@@ -1,1 +1,30 @@
 #include "Instructions/ZextInst.h"
+#include "BasicBlock.h"
+#include "Util.h"
+
+ZextInst::ZextInst(Type *destType, Value *value, BasicBlock *insertedBlock)
+    : Instruction(destType, InstId::zext, 1, insertedBlock),
+      destType_(destType) {
+  setOperand(value, 0);
+  insertedBlock->addInstruction(this);
+}
+
+ZextInst *ZextInst::Create(Type *destType, Value *value,
+                           BasicBlock *insertedBlock) {
+  return new ZextInst(destType, value, insertedBlock);
+}
+
+Type *ZextInst::getDestType() { return destType_; }
+
+std::string ZextInst::print() {
+  std::string IR;
+  char IRtemp[30];
+  // <result> = zext <type> <value> to <type2>
+  std::string fmt("%%%s = zext %s %s to %s");
+  std::snprintf(IRtemp, sizeof IRtemp, fmt.c_str(), getName().c_str(),
+                getOperandType(0)->getTypeName().c_str(),
+                print_as_op(getOperand(0)).c_str(),
+                destType_->getTypeName().c_str());
+  IR.assign(IRtemp);
+  return IR;
+}
