b21f42eb16f3f836ac8147898d00c1f40040c3e3
dongliushi <2445264666@qq.com>
2022-06-25 17:36:04 +0800

    mod IR's BinaryInst

--- IR/include/Instructions/BinaryInst.h
+++ IR/include/Instructions/BinaryInst.h
@@ -3,9 +3,27 @@
 
 class BinaryInst : public Instruction {
 public:
-  BinaryInst(Type *type, OpId opId, Value *valueL, Value *valueR,
-             BasicBlock *bb);
+  BinaryInst(Type *type, InstId instId, Value *leftValue, Value *rightValue,
+             BasicBlock *insertedBlock);
   std::string print() override;
+
+public:
+  static BinaryInst *CreateAdd(Context &context, Value *leftValue,
+                               Value *rightValue, BasicBlock *insertedBlock);
+  static BinaryInst *CreateSub(Context &context, Value *leftValue,
+                               Value *rightValue, BasicBlock *insertedBlock);
+  static BinaryInst *CreateMul(Context &context, Value *leftValue,
+                               Value *rightValue, BasicBlock *insertedBlock);
+  static BinaryInst *CreateSdiv(Context &context, Value *leftValue,
+                                Value *rightValue, BasicBlock *insertedBlock);
+  static BinaryInst *CreateFadd(Context &context, Value *leftValue,
+                                Value *rightValue, BasicBlock *insertedBlock);
+  static BinaryInst *CreateFsub(Context &context, Value *leftValue,
+                                Value *rightValue, BasicBlock *insertedBlock);
+  static BinaryInst *CreateFmul(Context &context, Value *leftValue,
+                                Value *rightValue, BasicBlock *insertedBlock);
+  static BinaryInst *CreateFdiv(Context &context, Value *leftValue,
+                                Value *rightValue, BasicBlock *insertedBlock);
 
 private:
 };
