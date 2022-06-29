6ee4acde8ad0ce327100bf69f01dedddb36808c6
aprill4 <liymane@126.com>
2022-06-29 16:00:46 +0800

    remove redundant * in load and store inst

--- IR/src/Instructions/LoadInst.cc
+++ IR/src/Instructions/LoadInst.cc
@@ -20,7 +20,7 @@ std::string LoadInst::print() {
   std::string IR;
   char IRtemp[30];
   // <result> = load <type>, <type>* <pointer>
-  std::string fmt("%%%s = load %s, %s* %s");
+  std::string fmt("%%%s = load %s, %s %s");
   assert(getOperandType(0)->isPointerType());
   std::snprintf(IRtemp, sizeof IRtemp, fmt.c_str(), getName().c_str(),
                 getOperandType(0)->getPtrElementType()->getTypeName().c_str(),
