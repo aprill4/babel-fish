eee2799780606de5cfa6e8e5df80f6e26d05cfbc
desperate <csb44027@126.com>
2022-06-29 17:24:08 +0800

    suppliment for BasicBlock functions

--- IR/src/BasicBlock.cc
+++ IR/src/BasicBlock.cc
@@ -23,6 +23,14 @@ void BasicBlock::addInstruction(Instruction *instruction) {
   instructionList_.emplace_back(instruction);
 }
 
+void BasicBlock::addPredecessor(BasicBlock* pre) {
+  predecessorBlocks_.emplace_back(pre);
+}
+
+void BasicBlock::addSuccessor(BasicBlock* suc) {
+  successorBlocks_.emplace_back(suc);
+}
+
 std::string BasicBlock::print() {
   std::string bb_ir;
   bb_ir += getName();
