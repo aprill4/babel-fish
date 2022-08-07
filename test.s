ac91a0fa5d8037787bafa7c4a571ea31d15d6bed
aprill4 <liymane@126.com>
2022-08-07 14:35:33 +0000

    ignore .s and .sy

--- test.s
+++ test.s
@@ -5,21 +5,34 @@
 .arm
 .global main
 main:
-main24:
+main27:
   mov	fp, sp
-  sub	sp, sp, #8
-  movw	vg0, #0
-  movt	vg0, #16448
-  vmov	vf1, vg0
-  vstr	vg0, [fp, #-4]
-  vldr	vf2, [fp, #-4]
-  mov	vg3, #1065353216
-  vmov	vf4, vg3
-  vadd.f32	vf5, vf2, vg3
-  vstr	vf5, [fp, #-8]
-  mov	r0, #1
-  add	sp, sp, #8
+  sub	sp, sp, #20
+  mov	vg0, #2
+  str	vg0, [fp, #-4]
+  mov	vg1, #1
+  str	vg1, [fp, #-8]
+  ldr	vg2, [fp, #-4]
+  ldr	vg3, [fp, #-8]
+  add	vg4, vg2, vg3
+  mov	vg5, #2
+  mul	vg6, vg4, vg5
+  str	vg6, [fp, #-12]
+  ldr	vg7, [fp, #-8]
+  sub	vg8, vg7, #1
+  str	vg8, [fp, #-16]
+  movw	vg9, #26214
+  movt	vg9, #16294
+  vmov	vf10, vg9
+  vstr	vg9, [fp, #-20]
   bx	lr
+  movw	vg11, #26214
+  movt	vg11, #16294
+  vmov.f32	s0, vg11
+  add	sp, sp, #20
+  vmov	vf12, vg11
 
 
 @ here are the globals +-+^_^+-=
+x:
+  .word	1
