9dac76dbf669c81871f085817bccbc8917cfa3df
aprill4 <liymane@126.com>
2022-08-07 14:18:01 +0000

    delete some rubbish

--- IR/include/Constant.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "User.h"
-
-class Constant : public User {
-public:
-  Constant(Context &c, Type *type, const std::string &name = "", std::size_t operandNum = 0)
-      : User(c, type, name, operandNum) {}
-};
--- IR/src/Use.cc
+++
@@ -1,7 +0,0 @@
-#include "Use.h"
-
-Use::Use(User *user,Value* value) : user_(user),value_(value) {}
-
-Value *Use::getValue() { return value_; }
-
-User *Use::getUser() { return user_; }
