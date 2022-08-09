768400947d68d584e302164397c77387accd9acf
dongliushi <2445264666@qq.com>
2022-08-09 15:24:46 +0800

    mod emit_gep

--- arm.cpp
+++ arm.cpp
@@ -989,9 +989,17 @@ void emit_gep(Instruction *inst, MachineBasicBlock* mbb) {
     auto gep = dynamic_cast<GetElementPtrInst*>(inst);
     auto res = make_vreg(MachineOperand::OperandType::Int, inst);
     auto ptr = gep->getOperand(0);
-    mbb->insts.emplace_back(
-            new Binary(
-                Binary::Int, Binary::ISub, res, new MReg(MReg::fp), new IImm(val_offset[ptr])));
+    if (!dynamic_cast<LoadInst*>(ptr)) {    
+        if (val_offset.find(ptr) != val_offset.end()) {        
+            mbb->insts.emplace_back(
+                    new Binary(
+                        Binary::Int, Binary::ISub, res, new MReg(MReg::fp), new IImm(val_offset[ptr])));
+        } else {
+            throw Exception("gep_error : about val_offset's ptr not find");
+        }
+    } else {
+        mbb->insts.emplace_back(new Mov(Mov::I2I, res, make_operand(ptr, mbb)));        
+    }
     auto element_type = ptr->getType();
     for (int i = 1; i < gep->getOperandNum(); i++) {
         auto item = gep->getOperand(i);
