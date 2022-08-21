ea6ebbfb2cb03ad7607c657b99383ba175db0c6a
aprill4 <liymane@126.com>
2022-08-21 10:50:20 +0800

    add remove identical mvs

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 set -eu
 
 mkdir -p build
-g++ -std=c++17 babel.cc lex.yy.cpp syntax_analyzer.tab.cpp ast.cpp IR.cpp Dominators.cc Mem2Reg.cc LoopSearch.cc LoopInvHoist.cc arm.cpp RemoveRedundantLoad.cpp -o build/babel -g
+g++ -std=c++17 babel.cc lex.yy.cpp syntax_analyzer.tab.cpp ast.cpp IR.cpp Dominators.cc Mem2Reg.cc LoopSearch.cc LoopInvHoist.cc arm.cpp remove_redundant_loads.cpp remove_identical_mvs.cpp -o build/babel -g
